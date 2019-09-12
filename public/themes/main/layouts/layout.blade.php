6924aedadc5859183b4f429aa7b6e76fd26f8c70
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-09-12 11:30:11 +0700

    add face like

--- public/themes/main/layouts/layout.blade.php
+++ public/themes/main/layouts/layout.blade.php
@@ -74,7 +74,8 @@
     </head>
 
     <body>
-
+		<div id="fb-root"></div>
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v4.0&appId=1506437109671064&autoLogAppEvents=1"></script>
         @content()
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
